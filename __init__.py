5dae8be717a8eb22144a8e81e4b1ecde785c5a9f
Benjamin David Killeen <benjamindkilleen@gmail.com>
2019-07-22 12:00:20 -0500

    updated __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,10 @@
+import os
 import tensorflow as tf
 
-sbnet_module = tf.load_op_library('sbnet_tensorflow/sbnet_ops/libsnet.so')
-from sbnet_module import reduce_mask, sparse_gather, sparse_scatter
+sbnet_module = tf.load_op_library(
+  os.path.join(os.path.dirname(__file__),
+               'sbnet_tensorflow/sbnet_ops/libsbnet.so'))
+reduce_mask = sbnet_module.reduce_mask
+sparse_gather = sbnet_module.sparse_gather
+sparse_scatter = sbnet_module.sparse_scatter
+sparse_scatter_var = sbnet_module.sparse_scatter_var
